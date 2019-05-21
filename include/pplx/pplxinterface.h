2d2c5601f4a1d14bbc8a6eb95b3b7a4b547c0b19
Jsonpersonal <50345994+Jsonpersonal@users.noreply.github.com>
2019-05-21 10:58:19 +0800

    Update pplxinterface.h

--- include/pplx/pplxinterface.h
+++ include/pplx/pplxinterface.h
@@ -28,6 +28,7 @@
 #ifdef _USE_REAL_ATOMICS
 #include <atomic>
 #endif
+#define __declspec(x) __attribute__ ((x))
 
 #if (defined(ANDROID) || defined(__ANDROID__))
 // This prevents users from requiring -Wno-attributes when using gcc-4.8 with the android NDK.
@@ -39,6 +40,52 @@
 namespace pplx
 {
   typedef void (_pplx_cdecl * TaskProc_t)(void *);
+  struct __declspec(novtable) scheduler_interface
+  {
+      virtual void schedule( TaskProc_t, _In_ void* ) = 0;
+  };
+  struct scheduler_ptr
+  {
+     /// <summary>
+    /// Creates a scheduler pointer from shared_ptr to scheduler
+    /// </summary>
+    explicit scheduler_ptr(std::shared_ptr<scheduler_interface> scheduler) : m_sharedScheduler(std::move(scheduler))
+    {
+        m_scheduler = m_sharedScheduler.get();
+    }
+
+    /// <summary>
+    /// Creates a scheduler pointer from raw pointer to scheduler
+    /// </summary>
+    explicit scheduler_ptr(_In_opt_ scheduler_interface * pScheduler) : m_scheduler(pScheduler)
+    {
+    }
+
+    /// <summary>
+    /// Behave like a pointer
+    /// </summary>
+    scheduler_interface *operator->() const
+    {
+        return get();
+    }
+
+    /// <summary>
+    ///  Returns the raw pointer to the scheduler
+    /// </summary>
+    scheduler_interface * get() const
+    {
+        return m_scheduler;
+    }
+
+    /// <summary>
+    /// Test whether the scheduler pointer is non-null
+    /// </summary>
+    operator bool() const { return get() != nullptr; }
+    private:
+    std::shared_ptr<scheduler_interface> m_sharedScheduler;
+    scheduler_interface * m_scheduler;
+  };
+  
 }// namespace pplx
 
 
