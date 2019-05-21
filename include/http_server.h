a25d5e03405c24635219d0253d5aaa414c7a96d5
Jsonpersonal <50345994+Jsonpersonal@users.noreply.github.com>
2019-05-21 14:11:50 +0800

    Update http_server.h

--- include/http_server.h
+++ include/http_server.h
@@ -17,9 +17,20 @@ namespace web{ namespace http{
 class http_server
 {
  public:
+  /// Release any held resources.
   virtual ~http_server(){};
-  
-  
+ /// Start listening for incoming requests.
+  virtual void start();
+  /// Registers an http listener.
+  virtual void register_listener(http_listener_impl *pListener) = 0;
+  /// Unregisters an http listener.
+  virtual void unregister_listener(http_listener_impl *pListener) = 0;
+   /// Stop processing and listening for incoming requests.
+  virtual pplx::task<void> stop() = 0;
+   /// Asynchronously sends the specified http response.
+   /// <param name="response">The http_response to send.</param>
+   /// <returns>A operation which is completed once the response has been sent.</returns>
+   virtual void respond(http::http_response response) = 0;
 };
 }
 }
