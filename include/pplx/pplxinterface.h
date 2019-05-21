6c61536f1906073f357c21cadd91ce04fbcdc6b5
Jsonpersonal <50345994+Jsonpersonal@users.noreply.github.com>
2019-05-21 10:22:47 +0800

    Create pplxinterface.h

---
+++ include/pplx/pplxinterface.h
@@ -0,0 +1,42 @@
+/****
+* =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+*
+* PPL interfaces
+*
+* For the latest on this and related APIs, please see http://casablanca.codeplex.com.
+*
+* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+****/
+#pargma once
+#ifndef _PPLXINTERFACE_H
+#define _PPLXINTERFACE_H
+
+#if (defined(_MSC_VER) && (_MSC_VER >= 1800)) && !CPPREST_FORCE_PPLX
+#error This file must not be included for Visual Studio 12 or later
+#endif
+
+#if defined(_CRTBLD)
+#elif defined(_WIN32)
+#if (_MSC_VER >= 1700)
+#define _USE_REAL_ATOMICS
+#endif
+#else // GCC compiler
+#define _USE_REAL_ATOMICS
+#endif
+
+#include <memory>
+#ifdef _USE_REAL_ATOMICS
+#include <atomic>
+#endif
+
+#if (defined(ANDROID) || defined(__ANDROID__))
+// This prevents users from requiring -Wno-attributes when using gcc-4.8 with the android NDK.
+#define _pplx_cdecl
+#else
+#define _pplx_cdecl __cdecl
+#endif
+
+
+
+#endif // _PPLXINTERFACE_H
+
