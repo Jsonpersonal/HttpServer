ad020bf5f34663111c721add1b28ee09ad2afa6e
Jsonpersonal <50345994+Jsonpersonal@users.noreply.github.com>
2019-05-21 09:15:16 +0800

    Update pplx.h

--- include/pplx/pplx.h
+++ include/pplx/pplx.h
@@ -1,10 +1,21 @@
 /****
 * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
 *
-* Parallel Patterns Library
+* Parallel Patterns Library 并行模式库
 *
 * For the latest on this and related APIs, please see http://casablanca.codeplex.com.
 *
 * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ****/
 #pragma once
+#ifndef _PPLX_H
+#define _PPLX_H
+
+#ifndef _WIN32
+#if defined(_WIN32) || defined(__cplusplus_winrt)
+#define _WIN32
+#endif
+#endif // _WIN32
+
+#endif //_PPLX_H
+
