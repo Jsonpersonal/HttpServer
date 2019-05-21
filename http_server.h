64b1898ac9fdbb2e3183c4d959c73369cbbbaabe
Jsonpersonal <50345994+Jsonpersonal@users.noreply.github.com>
2019-05-21 08:58:11 +0800

    Update http_server.h

--- http_server.h
+++ http_server.h
@@ -10,3 +10,13 @@
 #if _WIN32_WINNT < _WIN32_WINNT_VISTA
 #error "Error: http server APIs are not supported in XP"
 #endif //_WIN32_WINNT < _WIN32_WINNT_VISTA
+namespace web{ namespace http{
+/// <summary>
+/// Interface http listeners interact with for receiving and responding to http requests.
+/// </summary>
+class http_server
+{
+
+};
+}
+}
